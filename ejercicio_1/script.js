cba2cdedd08b0c350279324da6e24310de6a10b7
samserr1903-boop <samserr1903@gmail.com>
2026-08-10 18:45:23 -0500

    ejercicio 1 terminado

---
+++ ejercicio_1/script.js
@@ -0,0 +1,5 @@
+const colores = ["gris", "cafe", "negro", "rojo", "azul"];
+
+console.log(`Primer color: ${colores[0]}`);
+
+console.log(`Último color: ${colores[colores.length - 1]}`);
--- script.js
+++
@@ -1,6 +0,0 @@
-const colores = ["gris" , "cafe" ,"negro" ,"amarillo" , "verde"]
-
-
-console.log("primer color: ${colores [0]}");
-
-console.log("ultimo color: ${colores[colores.length -1]}");
